d79f0bd745f88a2cf0b50bade2e2df735a8dbee8
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2020-05-19 21:02:10 -0300

    Created simple PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController characterController;
+    PlayerController playerController;
+
+    private Vector3 movement;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
+        }
+        else
+        {
+            movement = new Vector3(1, -1, 1);
+        }
+
+        characterController.SimpleMove(movement);
+    }
+}
